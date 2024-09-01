915f1a1b7a4a69c42ddcc0216bc90c331d149a6d
Raed Masri <raed.bus.raed@gmail.com>
2024-09-01 19:26:11 +0300

    removing globe from contact page

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -1,29 +1,33 @@
 'use client';
 
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import SectionWrapper from '../hoc/SectionWrapper';
-import { GlobeDemo } from '../components/GlobeDemo';
+// import { GlobeDemo } from '../components/GlobeDemo';
 import Form from '../components/Form';
-import { slideIn } from '../utils/motion';
+// import { slideIn } from '../utils/motion';
 
 const ContactSection = () => (
 
   <div
-    className="py-8 md:py-12 lg:py-15 relative z-0 overflow-x-clip grid lg:grid-cols-2 grid-cols-1 gap-20 "
+    className="py-8 md:py-12 lg:py-15 relative z-0 overflow-x-clip flex flex-col"
   >
-    <motion.div
-      variants={slideIn('left', 'tween', 0.2, 1)}
-      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
-    >
-      <GlobeDemo />
-    </motion.div>
-    <motion.div
-      variants={slideIn('right', 'tween', 0.2, 1)}
-      className=" rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+    {/* <div
+    //   variants={slideIn('left', 'tween', 0.2, 1)}
+      className="xl:flex-1 xl:h-auto h-[150px]"
+    > */}
+    <div className=" text-center w-full h-[150px] mx-auto px-4">
+      <p className=" max-w-md  mx-auto text-white font-serif text-center mt-6 md:text-3xl text-2xl">
+        I&apos;m very flexible with <span className="bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">time zone</span> communications
+      </p>
+    </div>
+    <div
+    //   variants={slideIn('right', 'tween', 0.2, 1)}
+      className=" rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none px-8 pt-8 pb-0 md:pt-12  md:px-10 lg:pt-16 lg:px-20 sm:mx-12 md:mx-20 lg:mx-32 xl:mx-52 "
     >
       <Form />
-    </motion.div>
+    </div>
 
   </div>
+
 );
 export default SectionWrapper(ContactSection, 'contact');
